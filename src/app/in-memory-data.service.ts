69cef018c7ea64be8c7fdfa3f1b3e1758dcd9fb3
toshinari-ishii <59131143+toshinari-ishii@users.noreply.github.com>
2020-07-31 10:39:13 +0900

    practice-1

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -2,16 +2,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const members = [
-      { id: 1, name: '福士 義和' },
-      { id: 2, name: '仲里 淳矢' },
-      { id: 3, name: '高良真也' },
-      { id: 4, name: '具志　直' },
-      { id: 5, name: '新川 盛幸' },
-      { id: 6, name: '徳元 拓真' },
-      { id: 7, name: '金城　裕也' },
-      { id: 8, name: '宮里 優希' },
-      { id: 9, name: '大城 賢剛' },
-      { id: 10, name:'石井 俊成' }
+      { id: 9, name: '大城 賢剛' ,number:"S09963"},
+      { id: 5, name: '新川 盛幸' ,number:"T01530"},
+      { id: 6, name: '徳元 拓真' ,number:"S02869"},
+      { id: 3, name: '高良 真也' ,number:"T00944"},
+      { id: 8, name: '宮里 優希' ,number:"S06586"},
+      { id: 1, name: '福士 義和' ,number:"A14921"},
+      { id: 2, name: '仲里 淳矢' ,number:"T00114"},
+      { id: 7, name: '金城 裕也' ,number:"T00234"},
+      { id: 10, name:'石井 俊成' ,number:"S10659"},
+      { id: 4, name: '具志 直'  ,number:"T00070"}
     ];
     return { members };
   }
--- src/app/member.ts
+++ src/app/member.ts
@@ -1,4 +1,5 @@
 export class Member {
+  number: string;
   id: number;
   name: string;
 }
